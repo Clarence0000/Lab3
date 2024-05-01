1c8e3884a4652027943387254c4fd4b9bcf60af2
Clarence <CLARENCEENG.23@ichat.sp.edu.sg>
2024-05-01 12:36:28 +0700

    add employee_info_department

--- employee_info.py
+++ employee_info.py
@@ -22,7 +22,7 @@ def calculate_average_salary():
     #add your implementation to calculate here
     for item in employee_data:
         total += item["salary"]
-    
+
     average = total / len(employee_data)  
     return average
 
@@ -30,8 +30,9 @@ def get_employees_by_dept(department):
     result = []
 
     # Add your implementation from here
-
-
+    for item in employee_data:
+        if department == item["department"]:
+            result.append(item)
     return result
 
 def display_all_records():
